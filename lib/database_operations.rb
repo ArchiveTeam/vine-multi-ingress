cbe05555a4a95e79d7b5fa93f7e20ca6d7c00854
David Yip <yipdw@member.fsf.org>
2016-11-01 12:45:38 -0500

    Store tweets as separate documents.
    The number of tweets referencing some Vines is too large for the
    GET-modify-PUT cycle to be efficient.  We'll handle the counting in a
    view.

--- lib/database_operations.rb
+++ lib/database_operations.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 module DatabaseOperations
   def submit_video_with_retry(*args)
     submit_video(*args)
@@ -8,47 +10,20 @@ module DatabaseOperations
 
   def submit_video(tweet_id: nil, tweet_url: nil, vine_url:, now: Time.now.utc.iso8601, db:, credentials:)
     doc_id = format('video:%s', vine_url.gsub('/', '_'))
-    tweet_markers = make_tweet_markers(tweet_id, tweet_url)
-    doc = {
-      'url' => vine_url,
-      'tweets' => tweet_markers,
-      'created_at' => now
-    }
+    doc = { 'url' => vine_url, 'created_at' => now }
 
     resp = db.put(doc_id, doc, credentials)
 
-    if resp.success?
-      true
-    elsif resp.conflict?
-      resp = db.get!(doc_id, credentials)
-      doc = resp.body
-
-      if !doc['tweets']
-        doc['tweets'] = []
-      end
-
-      old_length = doc['tweets'].length
-      doc['tweets'] |= tweet_markers
-
-      if old_length != doc['tweets'].length
-        db.put!(doc_id, doc, credentials)
-      end
-
-      true
-    else
-      raise format('unhandled response: %d', resp.code)
+    if !(resp.conflict? || resp.success?)
+      raise "unhandled response: #{e.inspect}"
     end
-  end
 
-  def make_tweet_markers(tweet_id = nil, tweet_url = nil)
-    [].tap do |a|
-      if tweet_id
-        a << { 'id' => tweet_id }
-      end
+    if tweet_id
+      db.put!(format('tweet:%s', SecureRandom.uuid), { tweet_id: tweet_id, vine_url: vine_url }, credentials)
+    end
 
-      if tweet_url
-        a << { 'url' => tweet_url }
-      end
+    if tweet_url
+      db.put!(format('tweet:%s', SecureRandom.uuid), { tweet_url: tweet_url, vine_url: vine_url }, credentials)
     end
   end
 end
