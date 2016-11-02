2be3ab7fa2c5b6640d26c2985e27ad4b90ea5500
David Yip <yipdw@member.fsf.org>
2016-11-02 04:09:29 -0500

    Label tweet documents with the tweet ID and Vine video ID.
    A tweet can link to multiple Vines.

--- lib/database_operations.rb
+++ lib/database_operations.rb
@@ -9,22 +9,19 @@ module DatabaseOperations
   end
 
   def submit_video(tweet_id: nil, tweet_url: nil, vine_url:, now: Time.now.utc.iso8601, db:, credentials:)
-    doc_id = format('video:%s', vine_url.gsub('/', '_'))
+    vine_slug = vine_url.split('/').last
+    doc_id = format('video:%s', vine_slug)
     doc = { 'url' => vine_url, 'created_at' => now }
 
     resp = db.put(doc_id, doc, credentials)
 
-    if !(resp.conflict? || resp.success?)
-      raise "unhandled response: #{e.inspect}"
-    end
-
     if tweet_id
-      db.put!(format('tweet:%s', tweet_id), { tweet_id: tweet_id, vine_url: vine_url }, credentials)
+      db.put(format('tweet:%s:%s', tweet_id, vine_slug), { tweet_id: tweet_id, vine_url: vine_url }, credentials)
     end
 
     if tweet_url
       tweet_id = tweet_url.scan(/[0-9]+/).last
-      db.put!(format('tweet:%s', tweet_id), { tweet_url: tweet_url, vine_url: vine_url }, credentials)
+      db.put(format('tweet:%s:%s', tweet_id, vine_slug), { tweet_url: tweet_url, vine_url: vine_url }, credentials)
     end
   end
 end
